dc67186eba2cfaefff5efad45b86f60e68a3fb2c
Endre Szabo <github@urbnet.hu>
2012-04-08 13:08:20 +0000

    ０．０３　ｉｓ　ｏｕｔ，　ｓｏ　ｔｏ　ｓｐｅａｋ

--- mongolog.pl
+++ mongolog.pl
@@ -2,7 +2,7 @@ use strict;
 use vars qw($VERSION %IRSSI);
 use Irssi;
 
-$VERSION = '0.02';
+$VERSION = '0.03';
 %IRSSI = (
 	name        => 'mongolog',
 	authors     => 'Endre Szabo',
@@ -11,14 +11,15 @@ $VERSION = '0.02';
 	license     => 'GPL',
 );
 
-use Time::HiRes qw/gettimeofday/;
-use Storable qw/nfreeze thaw/;
+use Time::HiRes;
+use Storable;
 use MongoDB;
 use MongoDB::OID;
 
 my $conn;
 my $db;
 my $coll;
+my $insert_count;
 
 my %fields=(
 	'message own_private'		=> [qw/server_rec msg target orig_target/],
@@ -60,38 +61,41 @@ sub freeze_to_file($) {
 	my $temp=Irssi::settings_get_str('mongolog_tmp_file');
 	open(FD, '>>'.$temp) || die "Could not open tmp file '$temp' for dumping.";
 	Irssi::print('No DB connection, storing to temp file.') if $debug;
-	print FD unpack('H*',nfreeze(shift))."\n"; # is there a bettey way?
+	print FD unpack('H*',Storable::nfreeze(shift))."\n"; # is there a better way?
 	close FD;
 }
 
 sub store_log(@) {
 	my $debug=Irssi::settings_get_bool('mongolog_debug');
 	my %log; my $id;
-	$log{time}=join('.',gettimeofday());
-	$log{signal}=Irssi::signal_get_emitted();
+	$log{'time'}=join('.',Time::HiRes::gettimeofday());
+	$log{'signal'}=Irssi::signal_get_emitted();
 	Irssi::print("'$log{signal}' => '".join("', '",@_)."'.") if $debug;
-	@log{@{$fields{$log{signal}}}}=@_;
-	if(defined($log{server_rec})) {
-		$log{server}=$log{server_rec}->{tag};
-		delete($log{server_rec});
+	@log{@{$fields{$log{'signal'}}}}=@_;
+	if (defined($log{'server_rec'})) {
+		$log{server}=$log{'server_rec'}->{'tag'};
+		delete($log{'server_rec'});
 	}
-	if(defined($log{channel_rec})) {
-		$log{channel}=$log{channel_rec}->{name};
-		delete($log{channel_rec});
+	if (defined($log{'channel_rec'})) {
+		$log{channel}=$log{'channel_rec'}->{'name'};
+		delete($log{'channel_rec'});
 	}
-	if(defined($log{nick_rec})) {
-		$log{address}=$log{nick_rec}->{host};
-		$log{nick}=$log{nick_rec}->{nick};
-		delete($log{nick_rec});
+	if (defined($log{'nick_rec'})) {
+		$log{address}=$log{'nick_rec'}->{'host'};
+		$log{nick}=$log{'nick_rec'}->{'nick'};
+		delete($log{'nick_rec'});
 	}
-	if(defined($log{query_rec})) {
-		$log{nick}=$log{query_rec}->{name};
-		$log{server}=$log{query_rec}->{server_tag};
-		$log{address}=$log{query_rec}->{address};
-		delete($log{query_rec});
+	if (defined($log{'query_rec'})) {
+		$log{nick}=$log{'query_rec'}->{'name'};
+		$log{server}=$log{'query_rec'}->{'server_tag'};
+		$log{address}=$log{'query_rec'}->{'address'};
+		delete($log{'query_rec'});
 	}
+	my @tv=Time::HiRes::gettimeofday();
 	if($conn and $db and $coll and $id=$coll->insert(\%log)) {
-		Irssi::print("Stored log into db with _id '$id'.") if $debug;
+		$insert_count++;
+		my $elapsed=Time::HiRes::tv_interval(\@tv);
+		Irssi::print("Stored log into db with _id '$id' in $elapsed sec.") if $debug;
 	} else {
 		freeze_to_file(\%log);
 	}
@@ -104,6 +108,7 @@ sub mongodb_connect{
 		$conn=MongoDB::Connection->new(
 			host		=> Irssi::settings_get_str('mongolog_db_host'),
 			query_timeout	=> Irssi::settings_get_int('mongolog_db_query_timeout'),
+			safe		=> 1,
 		);
 	}; warn $@ if $@;
 	if(!$conn) {
@@ -122,11 +127,17 @@ sub mongodb_connect{
 	if (-e $temp) {
 		my $id;
 		my %log;
+		my @tv=gettimeofday();
+		my $item_count;
 		if (open(FD, '<'.$temp)) {
 			while(<FD>) {
-				$id=$coll->insert(thaw(pack('H*',$_)));
-				Irssi::print("temp file entry iserted into DB as id '$id'.") if $debug;
+				$id=$coll->insert(Storable::thaw(pack('H*',$_)));
+				$item_count++;
+				$insert_count++;
+				Irssi::print("Temp file entry #$item_count iserted into DB with _id '$id'.") if $debug;
 			}
+			my $elapsed=Time::HiRes::tv_interval(\@tv);
+			Irssi::print("Stored $item_count log entires from temp file in $elapsed sec.") if $debug;
 			close FD;
 			unlink $temp;
 		} else {
@@ -136,6 +147,11 @@ sub mongodb_connect{
 	Irssi::print('Connected to mongodb at '.Irssi::settings_get_str('mongolog_db_host'));
 	return 1;
 }
+
+sub mongolog_stat{
+	Irssi::print("inserted $insert_count events");
+
+};
 
 foreach my $signal (keys %fields) {
 	Irssi::signal_add_first($signal, 'store_log')
@@ -150,6 +166,7 @@ Irssi::settings_add_str('mongolog', 'mongolog_db_collection', 'logs');
 Irssi::settings_add_int('mongolog', 'mongolog_db_query_timeout', 2000);
 Irssi::settings_add_str('mongolog', 'mongolog_tmp_file', 'mongolog.tmp');
 Irssi::command_bind('mongolog_db_connect','mongodb_connect');
+Irssi::command_bind('mongolog_db_stats','mongolog_stat');
 
 Irssi::print("mongolog $VERSION started");
 mongodb_connect();
